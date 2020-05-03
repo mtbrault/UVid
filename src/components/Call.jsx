1498182eae36a268fc9b84bad25d4e6565e0acc0
Matthieu BRAULT <matthieu.brault@epitech.eu>
2020-05-03 17:28:48 +0200

    working for three people

--- src/components/Call.jsx
+++ src/components/Call.jsx
@@ -6,21 +6,28 @@ import '../styles/Call.css';
 const Call = ({ firebase, history }) => {
 	const { id } = useParams();
 	const [otherName, setOtherName] = useState('Non connecté');
+	const [thirdName, setThirdName] = useState('Non connecté');
 
 	useEffect(() => {
 		if (!id) history.push('/');
 		if (firebase.getName() === '')
 			history.push('/', { id });
 		else
-			firebase.joinRoom(id, setOtherName);
+			firebase.joinRoom(id, setOtherName, setThirdName);
 	}, [id, history, firebase]);
 
 	return (
 		<div style={{ display: "flex", justifyContent: "center" }}>
-			<video id="localVideo" ref={video => video && (video.srcObject = firebase.getLocalStream())} muted autoPlay playsInline ></video>
-			<p>{firebase.getName()}</p>
-			<video id="remoteVideo" ref={video => video && (video.srcObject = firebase.getRemoteStream())} autoPlay playsInline></video>
-			<p>{otherName}</p>
+			<div>
+				<video ref={video => video && (video.srcObject = firebase.getLocalStream())} muted autoPlay playsInline ></video>
+				<p>{firebase.getName()}</p>
+				<video ref={video => video && (video.srcObject = firebase.getRemoteStream())} autoPlay playsInline></video>
+				<p>{otherName}</p>
+			</div>
+			<div>
+				<video ref={video => video && (video.srcObject = firebase.getRemoteStream2())} autoPlay playsInline></video>
+				<p>{thirdName}</p>
+			</div>
 		</div>
 	);
 }
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -27,8 +27,10 @@ class Firebase {
 	constructor() {
 		firebase.initializeApp(firebaseConfig);
 		this.peerConnection = null;
+		this.peerConnection2 = null;
 		this.localStream = null;
 		this.remoteStream = null;
+		this.remoteStream2 = null;
 		this.name = '';
 	}
 
@@ -36,6 +38,7 @@ class Firebase {
 		const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
 		this.localStream = stream;
 		this.remoteStream = new MediaStream();
+		this.remoteStream2 = new MediaStream();
 	}
 
 	setName = name => { this.name = name }
@@ -46,6 +49,8 @@ class Firebase {
 
 	getRemoteStream = () => { return this.remoteStream; }
 
+	getRemoteStream2 = () => { return this.remoteStream2; }
+
 	createRoom = async () => {
 		const db = firebase.firestore();
 		const roomRef = await db.collection('rooms').doc();
@@ -55,7 +60,7 @@ class Firebase {
 		return roomRef.id;
 	}
 
-	joinRoom = async (id, setOtherName) => {
+	joinRoom = async (id, setOtherName, setThirdName) => {
 		const db = firebase.firestore();
 		const roomRef = db.collection('rooms').doc(`${id}`);
 		const snapshot = await roomRef.get();
@@ -65,8 +70,10 @@ class Firebase {
 			return false;
 		}
 		this.peerConnection = new RTCPeerConnection(configuration);
+		this.peerConnection2 = new RTCPeerConnection(configuration);
 		this.localStream.getTracks().forEach(track => {
 			this.peerConnection.addTrack(track, this.localStream);
+			this.peerConnection2.addTrack(track, this.localStream);
 		});
 		const offers = await snapshot.data();
 		const userId = Object.keys(offers).length;
@@ -75,20 +82,34 @@ class Firebase {
 			if (!event.candidate) return false;
 			calleeCandidatesCollection.add(event.candidate.toJSON());
 		});
+		this.peerConnection2.addEventListener('icecandidate', event => {
+			if (!event.candidate) return false;
+			calleeCandidatesCollection.add(event.candidate.toJSON());
+		});
 		this.peerConnection.addEventListener('track', event => {
 			event.streams[0].getTracks().forEach(track => {
 				this.remoteStream.addTrack(track);
 			});
 		});
+		this.peerConnection2.addEventListener('track', event => {
+			event.streams[0].getTracks().forEach(track => {
+				this.remoteStream2.addTrack(track);
+			});
+		});
 		if (userId === 0) {
 			const offer = await this.peerConnection.createOffer();
+			const offer2 = await this.peerConnection2.createOffer();
 			await this.peerConnection.setLocalDescription(offer);
+			await this.peerConnection2.setLocalDescription(offer2);
 			const answer = {
 				offer0: {
 					name: this.name,
 					offers: [{}, {
 						type: offer.type,
 						sdp: offer.sdp,
+					}, {
+						type: offer2.type,
+						sdp: offer2.sdp,
 					}],
 				}
 			};
@@ -97,8 +118,13 @@ class Firebase {
 				const data = snapshot.data();
 				if (!this.peerConnection.currentRemoteDescription && data && data.offer1) {
 					const rtcSessionDescription = new RTCSessionDescription(data.offer1.offers[0]);
-					setOtherName(data.offer1.name);
 					await this.peerConnection.setRemoteDescription(rtcSessionDescription);
+					setOtherName(data.offer1.name);
+				}
+				if (!this.peerConnection2.currentRemoteDescription && data && data.offer2) {
+					const rtcSessionDescription = new RTCSessionDescription(data.offer2.offers[0]);
+					await this.peerConnection2.setRemoteDescription(rtcSessionDescription);
+					setThirdName(data.offer2.name);
 				}
 			});
 			roomRef.collection(`calleeCandidates${1}`).onSnapshot(snapshot => {
@@ -109,17 +135,74 @@ class Firebase {
 					}
 				});
 			});
+			roomRef.collection(`calleeCandidates${2}`).onSnapshot(snapshot => {
+				snapshot.docChanges().forEach(async change => {
+					if (change.type === 'added') {
+						let data = change.doc.data();
+						await this.peerConnection2.addIceCandidate(new RTCIceCandidate(data));
+					}
+				});
+			});
 		} else if (userId === 1) {
 			await this.peerConnection.setRemoteDescription(new RTCSessionDescription(offers.offer0.offers[1]));
-			setOtherName(offers.offer0.name);
 			const answer = await this.peerConnection.createAnswer();
+			const offer2 = await this.peerConnection2.createOffer();
 			await this.peerConnection.setLocalDescription(answer);
+			await this.peerConnection2.setLocalDescription(offer2);
+			roomRef.onSnapshot(async snapshot => {
+				const data = snapshot.data();
+				if (!this.peerConnection2.currentRemoteDescription && data && data.offer2) {
+					const rtcSessionDescription = new RTCSessionDescription(data.offer2.offers[1]);
+					await this.peerConnection2.setRemoteDescription(rtcSessionDescription);
+					setThirdName(data.offer2.name);
+				}
+			});
 			const roomWithAnswer = {
 				offer1: {
 					name: this.name,
 					offers: [{
 						type: answer.type,
 						sdp: answer.sdp,
+					}, {}, {
+						type: offer2.type,
+						sdp: offer2.sdp,
+					}]
+				}
+			};
+			await roomRef.update(roomWithAnswer);
+			roomRef.collection(`calleeCandidates${0}`).onSnapshot(snapshot => {
+				snapshot.docChanges().forEach(async change => {
+					if (change.type === 'added') {
+						let data = change.doc.data();
+						await this.peerConnection.addIceCandidate(new RTCIceCandidate(data));
+					}
+				});
+			});
+			roomRef.collection(`calleeCandidates${2}`).onSnapshot(snapshot => {
+				snapshot.docChanges().forEach(async change => {
+					if (change.type === 'added') {
+						let data = change.doc.data();
+						await this.peerConnection2.addIceCandidate(new RTCIceCandidate(data));
+					}
+				});
+			});
+			setOtherName(offers.offer0.name);
+		} else if (userId === 2) {
+			await this.peerConnection.setRemoteDescription(new RTCSessionDescription(offers.offer0.offers[2]));
+			await this.peerConnection2.setRemoteDescription(new RTCSessionDescription(offers.offer1.offers[2]));;
+			const answer = await this.peerConnection.createAnswer();
+			const answer2 = await this.peerConnection2.createAnswer();
+			await this.peerConnection.setLocalDescription(answer);
+			await this.peerConnection2.setLocalDescription(answer2);
+			const roomWithAnswer = {
+				offer2: {
+					name: this.name,
+					offers: [{
+						type: answer.type,
+						sdp: answer.sdp,
+					}, {
+						type: answer2.type,
+						sdp: answer2.sdp,
 					}, {}]
 				}
 			};
@@ -132,6 +215,16 @@ class Firebase {
 					}
 				});
 			});
+			roomRef.collection(`calleeCandidates${1}`).onSnapshot(snapshot => {
+				snapshot.docChanges().forEach(async change => {
+					if (change.type === 'added') {
+						let data = change.doc.data();
+						await this.peerConnection2.addIceCandidate(new RTCIceCandidate(data));
+					}
+				});
+			});
+			setOtherName(offers.offer0.name);
+			setThirdName(offers.offer1.name);
 		}
 		return true;
 	}
